691a78f8c534ddff4d6b785633f73a568db8d0e9
Rova Fitia <codeurmdg@gmail.com>
2024-10-01 23:45:09 +0300

    Chore: init and install initial dependancy

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="font-semibold text-4xl">Hello World</h1>
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+  	extend: {
+      fontFamily: {
+        sans: ["var(--font-work-sans)"],
+      },
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+const { fontFamily } = require('tailwindcss/defaultTheme');
